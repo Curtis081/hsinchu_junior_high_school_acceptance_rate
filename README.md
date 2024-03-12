ba2681db58e5ada7cd14ff596c231f4c6f3aaf06
curtis081 <ar06053127hg@gmail.com>
2024-03-12 22:09:32 +0800

    initial

---
+++ README.md
@@ -0,0 +1,47 @@
+## Introduction(簡介)
+
+### Hsinchu Area Junior High School Entrance Exam Statistics
+#### Overview
+This Python script is designed to scrape and analyze admission statistics for junior high schools in the Hsinchu area from the website shirley.tw. Specifically, it focuses on collecting data related to the number of admitted students in Hsinchu Junior High School, Hsinchu Girls' High School, and Hsinchu Science and Engineering High School.
+
+#### Features
+* Fetches data from shirley.tw for the specified academic years.
+* Extracts information about the number of admitted students from the webpage.
+* Generates an Excel file containing the collected data.
+* Converts acceptance rates from string to float using pandas.
+* Styles and writes the data to an Excel file for better visualization.
+* Converts the Excel file to a PNG image for easy sharing and viewing.
+
+#### Output
+The script will create an "output_files" folder and store Excel files and corresponding PNG images inside it.
+
+###  新竹地區國中會考統計數據爬蟲
+#### 概述
+此Python腳本的目的是從 shirley.tw 網站中爬取新竹地區國中入學考試錄取統計數據。具體而言，它專注於收集有關新竹國中、新竹女中和新竹科工實中錄取人數的相關資料。
+
+#### 功能
+* 從 shirley.tw 網站上獲取指定學年度的資料。
+* 從網頁中提取有關錄取學生人數的信息。
+* 生成包含收集數據的Excel文件。
+* 使用pandas將錄取率從字符串轉換為浮點數。
+* 對數據進行風格設置並將其寫入Excel文件以便更好地視覺化。
+* 將Excel文件轉換為PNG圖片，以便輕鬆分享和查看。
+
+#### 輸出
+該腳本將創建一個名為 "output_files" 的文件夾，並在其中存儲Excel文件和相應的PNG圖像。
+
+## Author Information
+
+### Name
+Curtis Hsu(Hsu Ying-Hao)
+
+### Creation Date
+March 7, 2024
+
+### Contact Information
+- **Email**: ar06053127hga@hotmail.com.tw
+- **GitHub**: [CurtisHsuGitHub](https://github.com/Curtis081)
+- **LinkedIn**: [CurtisHsuLinkedIn](https://www.linkedin.com/in/yinghaohsu/)
+
+#### Note
+For any inquiries or contributions regarding this project, please feel free to contact me through the above channels. If reporting bugs or requesting features, consider opening an issue on GitHub for public tracking and documentation.
---
+++ main.py
@@ -0,0 +1,146 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from xlsxwriter import Workbook
+import pandas as pd
+from styleframe import StyleFrame, Styler
+import excel2img
+import sys
+import logging
+import warnings
+
+
+# Constants
+ACCEPTANCE_RATE_COL_ORIGINAL = '錄取率'
+ACCEPTANCE_RATE_COL = '錄取率%'
+
+# Setup logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[
+                        logging.FileHandler("app.log"),
+                        logging.StreamHandler(sys.stdout)
+                    ])
+
+
+def fetch_webpage_content(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        post_title_html = soup.find('h1')
+        if post_title_html is None:
+            logging.error('404: Page not found. Could not find the title <h1> tag in the HTML.')
+            return None, None
+
+        title = post_title_html.text
+        return soup, title
+    except requests.RequestException as e:
+        logging.error(f'Failed to fetch webpage content due to: {e}')
+        return None, None
+
+
+def create_and_save_excel(soup, title, output_folder):
+    # Ignore DeprecationWarning
+    warnings.filterwarnings("ignore", category=DeprecationWarning, module='openpyxl')
+
+    try:
+        excel_output_file_name = os.path.join(output_folder, title + '.xlsx')
+
+        workbook = Workbook(excel_output_file_name)
+        worksheet = workbook.add_worksheet()
+
+        table_html = soup.find('tbody')
+        td_htmls = table_html.find_all('td')
+
+        for td_html in td_htmls:
+            data_cell_id = td_html.get('data-cell-id')
+            data_original_value = td_html.get('data-original-value')
+            worksheet.write(data_cell_id, data_original_value)
+
+        workbook.close()
+        logging.info(f'Successfully created and saved Excel file: {excel_output_file_name}')
+        return excel_output_file_name
+    except Exception as e:
+        logging.error(f'Failed to create and save Excel file due to: {e}')
+        return None
+
+
+def convert_acceptance_rate(excel_output_file_name):
+    try:
+        df = pd.read_excel(excel_output_file_name)
+
+        acceptance_rate_slice_percentage = df[ACCEPTANCE_RATE_COL_ORIGINAL].str.slice(stop=-1)
+        acceptance_rate = pd.to_numeric(acceptance_rate_slice_percentage, errors='coerce')
+        df[ACCEPTANCE_RATE_COL] = acceptance_rate
+
+        df = df.sort_values(by=ACCEPTANCE_RATE_COL, ascending=False)
+        df = df.drop([ACCEPTANCE_RATE_COL_ORIGINAL], axis=1)
+
+        logging.info(f'Successfully converted acceptance rates in the DataFrame.')
+        return df, excel_output_file_name
+    except Exception as e:
+        logging.error(f'Failed to convert acceptance rates due to: {e}')
+        return None
+
+
+def style_and_write_to_excel(df, excel_output_file_name, sheet_name="Sheet1"):
+    try:
+        sf = StyleFrame(df, styler_obj=Styler(bg_color=None, bold=False, font='Arial', font_size=10.0, font_color=None,
+                                              number_format='General', protection=False, underline=None,
+                                              border_type='thin',
+                                              horizontal_alignment='left', vertical_alignment='center', wrap_text=True,
+                                              shrink_to_fit=True, fill_pattern_type='solid', indent=0.0,
+                                              comment_author=None,
+                                              comment_text=None, text_rotation=0))
+
+        with StyleFrame.ExcelWriter(excel_output_file_name) as writer:
+            sf.to_excel(writer, index=False, sheet_name=sheet_name, best_fit=list(df.columns.values))
+
+        logging.info(f'Successfully styled and write DataFrame to Excel: {excel_output_file_name}')
+        return sheet_name
+    except Exception as e:
+        logging.error(f'Failed to style and write DataFrame to Excel due to: {e}')
+
+
+def convert_excel_to_png(excel_file_name, post_title_html, sheet_name, output_folder):
+    output_png_file_name = os.path.join(output_folder, post_title_html + '.png')
+    try:
+        excel2img.export_img(excel_file_name, output_png_file_name, sheet_name, None)
+        logging.info(f'Successfully converted {excel_file_name} to {output_png_file_name}')
+    except Exception as e:
+        logging.error(f'Error converting {excel_file_name} to {output_png_file_name}: {e}')
+
+
+def create_output_folder():
+    output_folder = 'output_files'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    return output_folder
+
+
+def hsinchu_junior_high_school_acceptance_rate(academic_year):
+    url = f'https://shirley.tw/{academic_year}y-hsinchu-exam/'
+
+    soup, post_title_html = fetch_webpage_content(url)
+    if soup is None or post_title_html is None:
+        return
+
+    output_folder = create_output_folder()
+
+    excel_file_name = create_and_save_excel(soup, post_title_html, output_folder)
+    df, excel_output_file_name = convert_acceptance_rate(excel_file_name)
+
+    sheet_name = style_and_write_to_excel(df, excel_output_file_name)
+    convert_excel_to_png(excel_output_file_name, post_title_html, sheet_name, output_folder)
+
+    print('Process completed successfully.')
+
+
+if __name__ == '__main__':
+    for academic_year in range(110, 113):
+        try:
+            hsinchu_junior_high_school_acceptance_rate(str(academic_year))
+        except Exception as e:
+            logging.error(f'Error processing academic year {academic_year}: {e}')
+            continue
